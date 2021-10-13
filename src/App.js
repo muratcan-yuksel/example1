318d788367f8487b21ef50650a84a25b1f20c929
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-13 08:59:08 +0300

    icons part done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import Background from "./components/Background";
 import NavbarComp from "./components/NavbarComp";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "vivid-icons/dist/vivid-icons.min.js";
+import Icons from "./components/Icons";
+// import "./style/generalStyle.css";
 
 const App = () => {
   return (
     <div>
       <NavbarComp />
       <Background />
-      <i data-vi="doc"></i>
-      <i data-vi="doc" data-vi-size="96"></i>
+      <Icons />
     </div>
   );
 };
---
+++ src/components/Icons.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "vivid-icons/dist/vivid-icons.min.js";
+import "../style/iconStyle.css";
+
+const Icons = () => {
+  return (
+    <div>
+      <div className="iconContainer">
+        <div className="iconItem">
+          <i data-vi="hourglass" data-vi-size="96"></i>
+
+          <h3>Save Time</h3>
+          <p className="para">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+            voluptate modi fugiat cumque corrupti sapiente deleniti, laboriosam
+            incidunt enim tenetur vero similique fuga, maiores nisi nostrum quod
+            rem, laudantium nam.
+          </p>
+        </div>
+        <div className="iconItem">
+          <i data-vi="share" data-vi-size="96"></i>
+
+          <h3>Connect</h3>
+          <p className="para">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+            voluptate modi fugiat cumque corrupti sapiente deleniti, laboriosam
+            incidunt enim tenetur vero similique fuga, maiores nisi nostrum quod
+            rem, laudantium nam.{" "}
+          </p>
+        </div>{" "}
+        <div className="iconItem">
+          <i data-vi="rocket" data-vi-size="96"></i>
+
+          <h3>Scale</h3>
+          <p className="para">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+            voluptate modi fugiat cumque corrupti sapiente deleniti, laboriosam
+            incidunt enim tenetur vero similique fuga, maiores nisi nostrum quod
+            rem, laudantium nam.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Icons;
