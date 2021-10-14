71cdcb14feb2df3c5bd0ddc7fcdbbea2155a4587
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-14 13:50:11 +0300

    added a separator, dunno if it's good or nay tho

--- src/components/FormComp.js
+++ src/components/FormComp.js
@@ -6,28 +6,32 @@ import "../style/formStyle.css";
 
 const FormComp = () => {
   return (
-    <div id="container">
-      <Form id="form">
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Your name</Form.Label>
-          <Form.Control as="textarea" placeholder="Enter name" rows={1} />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Your message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
+    <div>
+      <div class="skewed"></div>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div id="container">
+        <Form id="form">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Your name</Form.Label>
+            <Form.Control as="textarea" placeholder="Enter name" rows={1} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Your message</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
